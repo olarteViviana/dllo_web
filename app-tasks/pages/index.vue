15d74a7d59220f1ed11898eaf29546063d2cbab2
olarteviviana <olarteviviana.a@gmail.com>
2023-09-19 17:55:01 -0500

    feat: login

--- app-tasks/pages/index.vue
+++ app-tasks/pages/index.vue
@@ -2,14 +2,61 @@
   <div>
     <center>
       <h1>Bienvenidos a la app de tareas</h1>
-      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+      <v-spacer></v-spacer>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        title="Registro de Usuario"
+      >
+        <v-container>
+          <v-text-field
+            v-model="first"
+            color="primary"
+            label="Primer Nombre"
+            variant="underlined"
+          ></v-text-field>
 
-      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
-<br>
-<br>
-      <NuxtLink to="/tareas">
-        Click para iniciar (Con nuxtLink)
-      </NuxtLink>
+          <v-text-field
+            v-model="last"
+            color="primary"
+            label="Apellido"
+            variant="underlined"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="email"
+            color="primary"
+            label="Email"
+            variant="underlined"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            color="primary"
+            label="Contraseña"
+            placeholder="Enter your password"
+            variant="underlined"
+          ></v-text-field>
+
+          <v-checkbox
+            v-model="terms"
+            color="secondary"
+            label="Acepto los términos y condiciones"
+          ></v-checkbox>
+          <a href="/iniciar"> Ya tengo cuenta </a>
+        </v-container>
+
+        <v-divider></v-divider>
+        
+        <v-card-actions>
+          <v-spacer></v-spacer>
+        
+          <v-btn color="success">
+            <a href="/tareas"> Ingresar</a>
+            <v-icon icon="mdi-chevron-right" end></v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </center>
 
   </div>
